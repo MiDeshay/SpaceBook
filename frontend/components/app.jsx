e586c046c1897acd6ba5f8f40a3cea1e7a55a387
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-09-24 15:05:20 -0700

    displays to page

---
+++ frontend/components/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+const App = () => (
+  <div>
+    <h1>Test</h1>
+    {/* <Switch>
+      <Route exact path="/" component={PostIndexContainer} />
+      <Route exact path="/posts/:postId" component={PostShowContainer} />
+      <Route path="/posts/:postId/edit" component={EditPostFormContainer} />
+    </Switch> */}
+  </div>
+);
+
+export default App;
---
+++ frontend/components/root.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import App from './app';
+
+const Root = () => (
+    // <Provider >
+    //   <HashRouter>
+    //     <App/>
+    //   </HashRouter>
+    // </Provider>
+    
+    <App/>
+  );
+  
+  export default Root;
+  
