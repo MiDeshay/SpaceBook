681d9062c76cc1a8fa803cb8dcbada79512aaec5
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-09-29 16:34:00 -0700

    rerouting to home

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import LoginFormContainer from './session_form/login_form_container';
 import HomePageContainer from "./home/home_page_container"
-import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import { Switch, Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import { AuthRoute, HomeAuthRoute } from '../util/route_util';
 
 const App = () => (
     <div>
         <Switch>
             <AuthRoute path="/login" component={LoginFormContainer}/>
-            <HomeAuthRoute path="/" component={HomePageContainer}/>
+            <HomeAuthRoute path="/home" component={HomePageContainer}/>
+            <Redirect from="/" to="home"/>
+            
         </Switch>
     </div>
 );
