039945b3e7a64c1d612eba01dfd610fe9c845561
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-10-08 04:23:56 -0700

    saving stuff

--- frontend/components/nav_bar/search_bar_container.jsx
+++ frontend/components/nav_bar/search_bar_container.jsx
@@ -1,10 +1,15 @@
 import { connect } from "react-redux";
 import SearchBar from "./search_bar";
+import { fetchAllUsers } from "../../actions/user_actions";
+
 const mSTP = (state) => ({
-    currentUser: state.entities.users[state.session.id]
+    currentUser: state.entities.users[state.session.id],
+    users: Object.values(state.entities.users)
+
 })
 
 const mDTP = (dispatch) => ({
+    fetchAllUsers: ()=> dispatch(fetchAllUsers())
     
 })  
 
