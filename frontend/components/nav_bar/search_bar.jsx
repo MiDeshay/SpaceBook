2eca8d3135b0907a936e4e37d844ad9bd250d907
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-10-08 04:25:02 -0700

    search bar

--- frontend/components/nav_bar/search_bar.jsx
+++ frontend/components/nav_bar/search_bar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class SearchBar extends React.Component{
     constructor(props){
         super(props)
-
+        this.state = {
+            searchTerm: ""
+        }
         this.firstPass = true;
         this.showSearch = this.showSearch.bind(this)
     }
@@ -11,6 +14,7 @@ class SearchBar extends React.Component{
     componentDidMount(){
         this.menu = document.getElementById(`search-bar-dropdown`);
         this.searchContents = document.getElementById("nav-search-bar");
+        this.props.fetchAllUsers()
 
         const that = this;
         document.addEventListener("click", (event) => { 
@@ -30,21 +34,51 @@ class SearchBar extends React.Component{
        
     }
 
+    handleSearchTerm(e){
+        this.setState({searchTerm: e.target.value})
+    }
+
 
     render(){
-        return(
-            <div id="search-bar-contents">
-                <button onClick={this.showSearch} id="search-button">
-                    <div id="search-logo-1"></div>
-                </button>
-                    
-                <div id="search-bar-dropdown">
-                    <div id="search-and-results">
-                        <div id="search-back-arrow"></div>
-                        <input placeholder="Search Spacebook" id="nav-search-bar"></input>
-                    </div>
+
+        const {searchTerm} = this.state
+
+        const display = <div id="search-bar-contents">
+        <button onClick={this.showSearch} id="search-button">
+            <div id="search-logo-1"></div>
+        </button>
+            
+        <div id="search-bar-dropdown">
+            <div id="search-and-results">
+                <div id="search-back-arrow"></div>
+                <input placeholder="Search Spacebook" id="nav-search-bar" onChange={this.handleSearchTerm.bind(this)}></input>
+                <div id='nav-search-results'>
+                    {this.props.users.filter(val => {
+                        if(searchTerm === ""){
+                            return ""
+                        } else if (val.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                        (val.lastName.toLowerCase().includes(searchTerm.toLowerCase()))){
+                            return val
+                        }
+                    }).map((user, key) => {
+                        return <li key={key}>
+                            <Link className="search-result" to={`/user/${user.id}`}>
+                                <button className="select-user">
+                                    <img className="search-pic" src={user.avatarUrl}></img>
+                                    <div className="search-name">{`${user.firstName} ${user.lastName}`}</div>
+                                    </button></Link>
+                        </li>
+
+                    })}
+
                 </div>
             </div>
+        </div>
+    </div>
+        return(
+            <>
+            {display}
+            </>
         )
     }
 }
